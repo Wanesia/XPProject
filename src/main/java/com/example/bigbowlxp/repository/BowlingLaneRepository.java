4cd38629e365acba6bf72704e5c455755ca4569e
zbahramzy <zalandbahramzy@gmail.com>
2022-09-27 11:28:43 +0200

    fixed the problem in BowlingLaneRepository.

--- src/main/java/com/example/bigbowlxp/repository/BowlingLaneRepository.java
+++ src/main/java/com/example/bigbowlxp/repository/BowlingLaneRepository.java
@@ -1,8 +1,9 @@
 package com.example.bigbowlxp.repository;
 
+import com.example.bigbowlxp.model.BowlingLane;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BowlingLaneRepository extends JpaRepository<BowlingLaneRepository, Integer> {
+public interface BowlingLaneRepository extends JpaRepository<BowlingLane, Integer> {
 }
