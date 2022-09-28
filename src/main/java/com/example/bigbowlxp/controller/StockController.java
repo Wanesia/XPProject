a5875604e051e0299729189f2926fdcb79a7d652
Octavian-Roman <work.octavian@gmail.com>
2022-09-28 09:51:34 +0200

    Adding Stock REST API

--- src/main/java/com/example/bigbowlxp/controller/StockController.java
+++ src/main/java/com/example/bigbowlxp/controller/StockController.java
@@ -1,12 +1,13 @@
 package com.example.bigbowlxp.controller;
 
+import com.example.bigbowlxp.model.BowlingBooking;
 import com.example.bigbowlxp.model.Stock;
 import com.example.bigbowlxp.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -23,5 +24,25 @@ public class StockController {
     @GetMapping
     public List<Stock> getStocks() {
         return stockService.getStocks();
+    }
+
+    @PostMapping
+    public void addStock(@RequestBody Stock stock) {
+        stockService.addStock(stock);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updateStock(
+            @PathVariable("id") Long id,
+            @RequestParam(required = false)
+                    String name,
+            @RequestParam(required = false)
+                    Integer quantity) {
+        stockService.updateStock(id, name, quantity);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteStock(@PathVariable("id") Long id) {
+        stockService.deleteStock(id);
     }
 }
--- src/main/java/com/example/bigbowlxp/service/StockService.java
+++ src/main/java/com/example/bigbowlxp/service/StockService.java
@@ -5,7 +5,9 @@ import com.example.bigbowlxp.repository.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class StockService {
@@ -23,5 +25,31 @@ public class StockService {
 
     public void addStock(Stock stock) {
         stockRepository.save(stock);
+    }
+
+    @Transactional
+    public void updateStock(Long id,
+                            String name,
+                            Integer quantity) {
+        Stock stock = stockRepository.findById(id).
+                orElseThrow(() -> new IllegalStateException("Stock with ID " + id + " does not exist."));
+        if (name != null
+                && !Objects.equals(stock.getName(), name)
+                && name.length() > 0) {
+            stock.setName(name);
+        }
+        if (quantity != null
+                && !Objects.equals(stock.getQuantity(), quantity)
+                && quantity >= 0) {
+            stock.setQuantity(quantity);
+        }
+    }
+
+    public void deleteStock(Long id) {
+        boolean exists = stockRepository.existsById(id);
+        if (!exists) {
+            throw new IllegalStateException("Stock with ID " + id + " does not exist.");
+        }
+        stockRepository.deleteById(id);
     }
 }
