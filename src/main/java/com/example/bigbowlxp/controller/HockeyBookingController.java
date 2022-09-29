e083408ffdce9509600050d7100d23031f15677f
Kimgmanis <Kimalexanderkeiser@gmail.com>
2022-09-29 20:53:42 +0200

    added CrossOrigin annotation to Hockey table and booking controller

--- src/main/java/com/example/bigbowlxp/controller/HockeyBookingController.java
+++ src/main/java/com/example/bigbowlxp/controller/HockeyBookingController.java
@@ -3,12 +3,14 @@ package com.example.bigbowlxp.controller;
 import com.example.bigbowlxp.model.HockeyBooking;
 import com.example.bigbowlxp.service.HockeyBookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "api/v1/hockey-booking")
 public class HockeyBookingController {
--- src/main/java/com/example/bigbowlxp/controller/HockeyTableController.java
+++ src/main/java/com/example/bigbowlxp/controller/HockeyTableController.java
@@ -3,12 +3,14 @@ package com.example.bigbowlxp.controller;
 import com.example.bigbowlxp.model.HockeyTable;
 import com.example.bigbowlxp.service.HockeyTableService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "api/v1/hockey-table")
 public class HockeyTableController {
