cc6daf526c84c929fa29bb967aa9cfbf69080f47
Daniel <dannysht27@gmail.com>
2022-09-30 12:40:08 +0200

    added cross origin in the backend

--- src/main/java/com/example/bigbowlxp/controller/DiningBookingController.java
+++ src/main/java/com/example/bigbowlxp/controller/DiningBookingController.java
@@ -3,6 +3,7 @@ package com.example.bigbowlxp.controller;
 import com.example.bigbowlxp.model.DiningBooking;
 import com.example.bigbowlxp.service.DiningBookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path="api/v1/dining-booking")
 public class DiningBookingController {
 
