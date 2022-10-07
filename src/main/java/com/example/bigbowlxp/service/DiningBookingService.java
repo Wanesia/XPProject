634c9f599b62be4a43e59c6af65e9b8091c3fe44
Daniel <dannysht27@gmail.com>
2022-10-07 14:07:17 +0200

    added @Transactional to enable put method

--- src/main/java/com/example/bigbowlxp/service/DiningBookingService.java
+++ src/main/java/com/example/bigbowlxp/service/DiningBookingService.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -29,6 +31,7 @@ public class DiningBookingService {
         diningBookingRepository.save(diningBooking);
     }
 
+    @Transactional
     public void updateDiningBooking(DiningBooking diningBooking)
     {
         diningBookingRepository.updateDiningBooking(diningBooking.getStartDateTime(), diningBooking.getId(), diningBooking.getEndDateTime());
